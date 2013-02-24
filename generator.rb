fe93c188494e0acf1c9e003552c22083438a2caf
av <av@fun-box.ru>
2013-02-24 23:11:47 +0400

    add annotations

--- generator.rb
+++ generator.rb
@@ -8,7 +8,8 @@ sensors = PulseMeter::Sensor::Configuration.new(
     sensor_type: 'timelined/counter',
     args: {
       interval: 1.hour,
-      ttl: 1.month
+      ttl: 1.month,
+      annotation: 'Purchases'
     }
   },
 
@@ -16,7 +17,8 @@ sensors = PulseMeter::Sensor::Configuration.new(
     sensor_type: 'timelined/counter',
     args: {
       interval: 5.minutes,
-      ttl: 1.week
+      ttl: 1.week,
+      annotation: 'Purchase api calls'
     }
   },
 
@@ -25,7 +27,8 @@ sensors = PulseMeter::Sensor::Configuration.new(
     args: {
       interval: 5.minutes,
       ttl: 1.week,
-      p: [0.9, 0.95, 0.99]
+      p: [0.9, 0.95, 0.99],
+      annotation: 'Purchase api calls'
     }
   },
 
@@ -33,7 +36,8 @@ sensors = PulseMeter::Sensor::Configuration.new(
     sensor_type: 'timelined/counter',
     args: {
       interval: 1.minute,
-      ttl: 1.week
+      ttl: 1.week,
+      annotation: 'Web requests'
     }
   },
 
@@ -42,33 +46,34 @@ sensors = PulseMeter::Sensor::Configuration.new(
     args: {
       interval: 1.minute,
       ttl: 1.week,
-      p: [0.9, 0.95, 0.99]
+      p: [0.9, 0.95, 0.99],
+      annotation: 'Web request time'
     }
   },
 
   web_request_ff_1day: {
     sensor_type: 'timelined/uniq_counter',
-    args: {interval: 1.day, ttl: 1.month}
+    args: {interval: 1.day, ttl: 1.month, annotation: 'Firefox'}
   },
   web_request_chrome_1day: {
     sensor_type: 'timelined/uniq_counter',
-    args: {interval: 1.day, ttl: 1.month}
+    args: {interval: 1.day, ttl: 1.month, annotation: 'Chrome'}
   },
   web_request_ie_1day: {
     sensor_type: 'timelined/uniq_counter',
-    args: {interval: 1.day, ttl: 1.month}
+    args: {interval: 1.day, ttl: 1.month, annotation: 'IE'}
   },
   web_request_opera_1day: {
     sensor_type: 'timelined/uniq_counter',
-    args: {interval: 1.day, ttl: 1.month}
+    args: {interval: 1.day, ttl: 1.month, annotation: 'Opera'}
   },
   web_request_mobile_1day: {
     sensor_type: 'timelined/uniq_counter',
-    args: {interval: 1.day, ttl: 1.month}
+    args: {interval: 1.day, ttl: 1.month, annotation: 'Mobile'}
   },
   web_request_other_1day: {
     sensor_type: 'timelined/uniq_counter',
-    args: {interval: 1.day, ttl: 1.month}
+    args: {interval: 1.day, ttl: 1.month, annotation: 'Other'}
   }
 
 )
