ef5ee5993c3efbd1bca23bbf2aa03183a7febb0b
av <av@fun-box.ru>
2016-09-01 20:02:08 +0300

    Ansible deploy

--- unicorn.rb
+++ unicorn.rb
@@ -1,4 +1,4 @@
-APP_PATH = "/home/av/pulse_meter_demo"
+APP_PATH = "/home/av/pulse-meter.rubybox.ru"
 CURRENT = File.join(APP_PATH, 'current')
 SHARED = File.join(APP_PATH, 'shared')
 
@@ -7,7 +7,7 @@ preload_app false
 timeout 30
 
 working_directory CURRENT
-listen "#{CURRENT}/tmp/unicorn.sock", :backlog => 2048  
+listen "#{CURRENT}/tmp/unicorn.sock", :backlog => 2048
 
 PID = "#{SHARED}/pids/unicorn.pid"
 pid PID
